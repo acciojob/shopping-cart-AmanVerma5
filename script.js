f3b944dc9c236ff436327bc494fb3b766dceba70
AmanVerma5 <amanv4775@gmail.com>
2023-09-08 11:32:09 +0530

    submitting code-Fri Sep 08 2023 11:32:03 GMT+0530 (India Standard Time)

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+let name=document.getElementById('item-name-input')
+let price=document.getElementById('item-price-input')
+let button=document.getElementById('add')
+
+button.addEventListener('click',()=>{
+    let item_price=parseFloat(price.value);
+    let item_name=name.value;
+   
+    let tr=document.createElement('tr');
+    tr.innerHTML=`<td>${item_name}</td>
+                  <td>${item_price}</td>`;
+
+    let table=document.getElementsByTagName('table')[0];
+    table.append(tr);
+})
+
